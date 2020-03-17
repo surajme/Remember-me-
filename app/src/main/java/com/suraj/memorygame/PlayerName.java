fa87be8919d2921082465c45025c2773352e86b3
Apple <apple@123-macbook-pro.lan>
2020-03-17 16:25:08 +0530

    edittext validation

--- app/src/main/java/com/suraj/memorygame/PlayerName.java
+++ app/src/main/java/com/suraj/memorygame/PlayerName.java
@@ -1,16 +1,19 @@
 package com.suraj.memorygame;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -20,6 +23,8 @@ public class PlayerName extends Fragment {
     private EditText playerName_et;
     private Button btnPlay;
     private String playerName;
+    private static final String TAG = "MainActivity";
+
 
 
 
@@ -29,14 +34,32 @@ public class PlayerName extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_player_name, container, false);
 
-                playerName_et = (EditText)rootView.findViewById(R.id.name_et);
 
-
+        playerName_et = (EditText)rootView.findViewById(R.id.name_et);
+//        String xyz = playerName_et.getText().toString();
         Button btnFragment = (Button) rootView.findViewById(R.id.btnPlay);
+
+
+
+
+//        if(playerName.isEmpty()) {
+//            btnFragment.setEnabled(false);
+//        } else {
+//            btnFragment.setEnabled(true);
+//        }
+
+
         btnFragment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
               playerName = playerName_et.getText().toString();
+
+                if(TextUtils.isEmpty(playerName)) {
+                    playerName_et.setError("Don't act smart!");
+                    return;
+                }else {
+                    //no content
+                }
 
                 FragmentTransaction fr = getFragmentManager().beginTransaction();
                 fr.replace(R.id.layoutFragment, new EasyLevel());
@@ -59,39 +82,3 @@ public class PlayerName extends Fragment {
 
 }
 
-//    private EditText memail_et,muser_name_et,mphone_et;
-//    private Button sign_up_btn;
-//    private String memail,muser_name,mphone_number;
-//    @Override
-//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-//                             Bundle savedInstanceState) {
-//        // Inflate the layout for this fragment
-//        View rootView=inflater.inflate(R.layout.fragment_main_activity, container, false);
-//        memail_et=(EditText)rootView.findViewById(R.id.email_et);
-//        muser_name_et=(EditText)rootView.findViewById(R.id.user_name_et);
-//        mphone_et=(EditText)rootView.findViewById(R.id.mobile_no_et);
-//        sign_up_btn=(Button)rootView.findViewById(R.id.sign_up_btn);
-//        sign_up_btn.setOnClickListener(mClickListener);
-//        return rootView;
-//    }
-//    View.OnClickListener mClickListener=new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//
-//            memail=memail_et.getText().toString();
-//            muser_name=muser_name_et.getText().toString();
-//            mphone_number=mphone_et.getText().toString();
-//            FragmentTransaction transection=getFragmentManager().beginTransaction();
-//            SecondFragment mfragment=new SecondFragment();
-
-////using Bundle to send data
-//            Bundle bundle=new Bundle();
-//            bundle.putString("email",memail);
-//            bundle.putString("user_name",muser_name);
-//            bundle.putString("phone",mphone_number);
-//            mfragment.setArguments(bundle); //data being send to SecondFragment
-//            transection.replace(R.id.main_fragment, mfragment);
-//            transection.commit();
-//        }
-//    }
-//}
